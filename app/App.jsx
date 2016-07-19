aa107f51adcb51cabf5598b82fe179d663f012de
shinima <842351815@qq.com>
2016-07-19 14:36:12 +0800

    新增帮助

--- app/App.jsx
+++ app/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Controller from './components/Controller.jsx'
 import Board from './components/Board.jsx'
+import Help from './components/Help.jsx'
 import './styles/app.styl'
 
 const App = () => (
   <div className="app">
     <Controller />
     <Board />
+    <Help />
   </div>
 )
 
---
+++ app/components/Help.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Help = () => (
+  <div className="help">
+    <h1>操作说明</h1>
+    <p>A 左移</p>
+    <p>D 右移</p>
+    <p>S 下移</p>
+    <p>W 逆时针旋转</p>
+    <p>SPACE 下落</p>
+  </div>
+)
+
+
+export default Help
